8c2d76db0d772f50a8027b8f49e02bbcea9584db
Clement Kiritchouk <clement.kiritchouk@gmail.com>
2025-04-25 16:07:15 +0200

    update ts config

--- src/utils/get-pubspec.ts
+++ src/utils/get-pubspec.ts
@@ -1,12 +1,12 @@
 import * as yaml from "js-yaml";
 import { getPubspecPath } from "./get-pubspec-path";
-import { workspace, Uri } from "vscode";
+import * as vscode from "vscode";
 
 export async function getPubspec () {
   const pubspecPath = getPubspecPath();
   if (pubspecPath) {
     try {
-      let content = await workspace.fs.readFile(Uri.file(pubspecPath));
+      let content = await vscode.workspace.fs.readFile(vscode.Uri.file(pubspecPath));
       return yaml.safeLoad(content.toString());
     } catch (_) { }
   }
